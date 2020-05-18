867ce065ae9158be68fea235f2a1546958489076
bmsmx <mvg@bms.mx>
2020-05-18 18:36:42 -0500

    V0.91

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ add_shortcode('bms_form1','bms_form1');
 
     function bms_form1() 
 {
-    wp_enqueue_style('css_aspirante', plugins_url('style.css', __FILE__)); 
+    wp_enqueue_style('css_aspirante', plugins_url('style.css')); 
 
     ob_start(); 
 
